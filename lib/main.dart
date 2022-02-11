5ea88848bc3eb608955cf1e9339b179dd4ebbec9
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-02-12 02:51:59 +0530

    added colors and cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:ffi';
-
+import 'package:velocity_x/velocity_x.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+
 void main() {
   runApp(const CountUp());
 }
+
 class CountUp extends StatefulWidget {
   const CountUp({Key? key}) : super(key: key);
 
@@ -29,7 +32,7 @@ class _CountUpState extends State<CountUp> {
     setState(() {
       final SECONDS = duration.inSeconds + addSeconds;
 
-      duration = Duration(seconds: SECONDS); 
+      duration = Duration(seconds: SECONDS);
     });
   }
 
@@ -39,14 +42,34 @@ class _CountUpState extends State<CountUp> {
 
   @override
   Widget build(BuildContext context) {
+    final style = TextStyle(
+              // color: Color.fromRGBO(7, 59, 76, 1),
+              color: Colors.black,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // title: 'Welcome to Flutter'
       home: Scaffold(
+        // backgroundColor: Color.fromRGBO(255, 112, 165, 1),
+        backgroundColor: Colors.amber,
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+        // backgroundColor: Color.fromRGBO(255, 112, 165, 1),
+        backgroundColor: Colors.amber,
+          elevation: 0.0,
+          title: Text(
+            'Timer',
+            style: GoogleFonts.amiko(
+              textStyle: style,
+            ),
+          ),
         ),
-        body: Center(
-          child: buildTime(),
+        body: Container(
+          padding: EdgeInsets.fromLTRB(30, 60, 10, 30),
+          child: Center(
+            child: buildTime(),
+          ),
         ),
       ),
     );
@@ -57,13 +80,52 @@ class _CountUpState extends State<CountUp> {
     // if 11 --> 11
 
     String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
 
-
-    return Text(
-      '$minutes:$seconds',
-      style: TextStyle(fontSize: 80 ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        buildTimeCard(time: hours, header: 'Hours'),
+        const SizedBox(width: 8),
+        buildTimeCard(time: minutes, header: 'Minutes'),
+        const SizedBox(width: 8),
+        buildTimeCard(time: seconds, header: 'Seconds'),
+      ],
     );
   }
 }
+
+final style2 = TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              // color: Colors.black,
+              fontSize: 72,
+            );
+
+Widget buildTimeCard({required String time, required String header}) => Column(
+      children: [
+        Container(
+          constraints: BoxConstraints(minWidth: 100, maxHeight: 100),
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            // color: Color.fromRGBO(255, 175, 204, 1),
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text(
+            time,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.amiko(
+              textStyle: style2,
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        // Text(
+        //   header,
+        //   textScaleFactor: 1.5,
+        // )
+      ],
+    );
